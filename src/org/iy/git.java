408aa7c1a1f4d4d78d7c44f13b0de6a55637bf5d
sadhracsamraj <sadhracsamrajkbm@gmail.com>
2021-10-08 12:13:41 +0530

    task are underprocess

--- src/org/iy/git.java
+++ src/org/iy/git.java
@@ -1,5 +1,11 @@
 package org.iy;
 
 public class git {
-
+private static void username() {
+	// TODO Auto-generated method stub
+System.out.println("sam");
+}
+public static void main(String[] args) {
+	username();
+}
 }
